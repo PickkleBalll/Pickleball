7dc7027a9cfa127d095e634e235ecb6cabf89f31
Thaodungnoo <dungtt0605@gmail.com>
2025-05-24 01:26:50 +0700

    init FE and BE

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center">
+        <p className="text-4xl text-red-500">Hello!!!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
