03de7054bc7303cad1e6a2755e72491241c87f45
Nguyen Thi Hong Phuc <phucnth3110@ut.edu.vn>
2025-05-28 15:25:47 +0700

    add background for sign in

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -17,62 +17,12 @@ const SignIn = () => {
   };
 
   return (
-    // <div className="sign-in-page min-h-screen flex items-center justify-center bg-gray-100">
-    //   <div className="bg-white shadow-lg rounded-3xl flex max-w-5xl w-full overflow-hidden">
-    //     {/* Left: Form */}
-    //     <div className="flex-1 p-10 flex flex-col justify-center">
-    //       <h2 className="text-2xl font-semibold mb-6">Welcome Back</h2>
-    //       <form onSubmit={handleLogin} className="space-y-4">
-    //         <input
-    //           type="text"
-    //           placeholder="Username"
-    //           value={email}
-    //           onChange={(e) => setEmail(e.target.value)}
-    //           required
-    //           className="w-full px-4 py-3 border border-gray-300 rounder-full focus:outline-none focus:ring-2 focus:ring-blue-400"
-    //         />
-
-    //         <input 
-    //           type="password"
-    //           placeholder="Password"
-    //           value={password}
-    //           onChange={(e) => setPassword(e.target.value)}
-    //           required
-    //           className="w-full px-4 py-3 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400 btn-submit"
-    //         />
-
-    //         <div className="text-sm text-gray-500">
-    //           <a href="#" className="hover:underline">
-    //             Forgot Password ?
-    //           </a>
-    //         </div>
-
-    //         <button
-    //            type="submit"
-    //            className="w-full py-3 rounder-full text-white font-semibold transition bg-gradient-to-r from-green-300 to-blue-400 hover:opacity-90 rounded-4xl btn-submit"
-    //            >
-    //             SIGN IN 
-    //         </button>
-    //         </form>
-
-    //         <div className="mt-6 text-sm text-center text-gray-300">
-    //           <a href="#" className="hover:underline">
-    //             Create your account →
-    //           </a>
-    //         </div>
-    //       </div>
-
-    //       {/* Right: Image (bo tròn) */}
-    //     <div className="hidden md:block flex-1 bg-cover bg-center relative" style={{ backgroundImage: "url(' ')" }}>
-    //       <div className="absolute inset-0 bg-black opacity-10 rounded-bl-[80px] rounded-tl-full rounded-br-full"></div>
-    //     </div>
-    //   </div>
-    // </div>
-
+    
+     // Left pagepage
     <div className="sign-in-page flex">
-      <div className="w-[30%] p-4 bg-blue-100 flex align-items-center">
+      <div className="w-[40%] p-4 bg-white-200 flex align-items-center">
         <div className="flex-1 p-10 flex flex-col justify-center">
-          <h2 className="text-2xl font-semibold mb-6 text-center">Welcome Back</h2>
+          <h2 className="text-3xl font-semibold mb-6 text-center">Welcome Back</h2>
           <form onSubmit={handleLogin} className="space-y-4">
             <input
               type="text"
@@ -80,7 +30,8 @@ const SignIn = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
-              className="w-full px-4 py-3 border border-gray-300 rounder-full focus:outline-none focus:ring-2 focus:ring-blue-400 input-form"
+              className="w-full px-4 py-3 border border-gray-300 rounder-full focus:outline-none focus:ring-2 focus:ring-blue-400 
+              input-form"
             />
 
             <input 
@@ -92,7 +43,7 @@ const SignIn = () => {
               className="w-full px-4 py-3 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400 input-form "
             />
 
-            <div className="text-sm text-gray-500">
+            <div className="text-sm text-black-500">
               <a href="#" className="hover:underline">
                 Forgot Password ?
               </a>
@@ -100,25 +51,21 @@ const SignIn = () => {
 
             <button
                type="submit"
-               className="w-full py-3 rounder-full text-white font-semibold transition bg-gradient-to-r from-green-300 to-blue-400 hover:opacity-90 rounded-4xl btn-submit"
+               className="w-full py-3 rounder-full text-black font-semibold transition bg-gradient-to-r from-green-300 to-blue-400 hover:opacity-90 rounded-4xl button-sign-in"
                >
                 SIGN IN 
             </button>
             </form>
 
-            <div className="mt-6 text-sm text-center text-gray-300">
-              <a href="#" className="hover:underline">
+            <div className="mt-6 text-sm text-center text-black-300">
+              <a href="#" className="hover:underline ">
                 Create your account →
               </a>
             </div>
           </div>
       </div>
-      <div className="w-[70%] p-4 bg-blue-200">Column 2 (70%)
-        <div className="hidden md:block flex-1 bg-cover bg-center relative sign-in-background" >
-          <div className="absolute inset-0 bg-black opacity-10 rounded-bl-[80px] rounded-tl-full rounded-br-full"></div>
+        <div className="w-[60%] h-screen bg-cover bg-center rounded-l-[250px] sign-in-background"></div> 
         </div>
-      </div>
-    </div>
 
   );
 };
