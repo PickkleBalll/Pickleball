c303926ba031c7f630a49319b1e224108fb839d1
Nguyen Thi Hong Phuc <phucnth3110@ut.edu.vn>
2025-05-29 10:01:24 +0700

    add sign up page

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -14,12 +14,16 @@ const SignIn = () => {
     // Giả lập xác thực thành công
     localStorage.setItem('auth', 'true');
     navigate('/dashboard');
-  };
 
+
+  };
+  const handleGoToSignUp = () => {
+    navigate('/signup'); // Điều hướng sang trang Sign up 
+  };
   return (
-    
-     // Left pagepage
-    <div className="sign-in-page flex">
+
+    // Left page
+    <div className="auth-page flex">
       <div className="w-[40%] p-4 bg-white-200 flex align-items-center">
         <div className="flex-1 p-10 flex flex-col justify-center">
           <h2 className="text-3xl font-semibold mb-6 text-center">Welcome Back</h2>
@@ -30,17 +34,17 @@ const SignIn = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
-              className="w-full px-4 py-3 border border-gray-300 rounder-full focus:outline-none focus:ring-2 focus:ring-blue-400 
-              input-form"
+              className="w-full px-4 py-3 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400 
+              auth-input"
             />
 
-            <input 
+            <input
               type="password"
               placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
-              className="w-full px-4 py-3 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400 input-form "
+              className="w-full px-4 py-3 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400 auth-input"
             />
 
             <div className="text-sm text-black-500">
@@ -50,22 +54,23 @@ const SignIn = () => {
             </div>
 
             <button
-               type="submit"
-               className="w-full py-3 rounder-full text-black font-semibold transition bg-gradient-to-r from-green-300 to-blue-400 hover:opacity-90 rounded-4xl button-sign-in"
-               >
-                SIGN IN 
+              type="submit"
+              className="w-full py-3 rounded-full text-black font-semibold transition bg-gradient-to-r from-green-300 to-blue-400 hover:opacity-90 rounded-4xl auth-button"
+            >
+              SIGN IN
             </button>
-            </form>
+          </form>
+          <button
+            onClick={handleGoToSignUp}
+            className="mt-6 text-sm text-center text-black-300 hover:underline"
+          >
+            Create your account →
+          </button>
 
-            <div className="mt-6 text-sm text-center text-black-300">
-              <a href="#" className="hover:underline ">
-                Create your account →
-              </a>
-            </div>
-          </div>
-      </div>
-        <div className="w-[60%] h-screen bg-cover bg-center rounded-l-[250px] sign-in-background"></div> 
         </div>
+      </div>
+      <div className="w-[60%] h-screen bg-cover bg-center rounded-l-[250px] auth-background"></div>
+    </div>
 
   );
 };
