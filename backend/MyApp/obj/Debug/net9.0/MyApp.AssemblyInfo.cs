d1c208ecb1b9becd5b90abcedd88a0a50435c831
Lê Huỳnh Cao Dương <caoduong22102004@gmail.com>
2025-06-01 14:13:54 +0700

    backend authen,admin

--- backend/MyApp/obj/Debug/net9.0/MyApp.AssemblyInfo.cs
+++ backend/MyApp/obj/Debug/net9.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+897d76cbd9429f0691dcd0b13f666beca3901633")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8238faa5e2ef2d45fe6c76bf680bb4bc80cb35f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
