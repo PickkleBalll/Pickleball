06b97530da735f6cadcd5216af33ac721ad7a540
Nguyen Van Manh <manhnv0040@ut.edu.vn>
2025-06-15 13:45:29 +0700

    chinh sua

--- backend/PickleballVideoAnalyzer/PickleballVideoAnalyzer/Program.cs
+++ backend/PickleballVideoAnalyzer/PickleballVideoAnalyzer/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Them Swagger service
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+// Bat Swagger khi debug
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.MapControllers();
 app.Run();
